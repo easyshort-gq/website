1a0965f1b8791d55d4589a533ffa61aa444c018d
Alessandro <77743955+ale-006@users.noreply.github.com>
2022-03-03 19:18:25 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -11,10 +11,14 @@ module.exports = {
         name: "report"
     },
     async execute(interaction) {
+        try {
         let url = interaction.options.getString("url")
         let reason = interaction.options.getString("reason")
         if(!db.has(url)) return interaction.reply({content: "You have provided an URL that does not exist.", ephemeral: true})
         webhook.send("<a:arrow:895720936312033281> **__NEW REPORT__**\n\n__URL:__ [" + url + "](<" + db.get(url) + ">)\n__Reason:__ " + reason + "\n__User:__ " + interaction.user.tag)
         interaction.reply({content: "<a:arrow:895720936312033281> **Your report was successfully sent! Join the [this server](<https://discord.gg/X4nGGPvJJY>) to stay updated on the progress of the removal request.**", ephemeral: true})
+        } catch (err) {
+            console.log(err)
+        }
     }
 }

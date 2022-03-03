d3bc232b95c6926d5498f004f8979859ec956106
Alessandro <77743955+ale-006@users.noreply.github.com>
2022-03-03 19:20:04 +0100

    Update delete-url.js

--- commands/delete-url.js
+++ commands/delete-url.js
@@ -9,9 +9,13 @@ module.exports = {
         name: "delete-url"
     },
     async execute(interaction) {
+        try {
         if(interaction.user.id != "766336575793135697") return interaction.reply({content: "You can't use this developer-only command.", ephemeral: true})
         let url = interaction.options.getString("url")
         db.delete(url)
         interaction.reply({content: "The `" + url + "` was successfully deleted!", ephemeral: true})
+        } catch (err) {
+            console.log(err)
+        }
     }
 }

f8d9154535e5fcf719ff54b2ca16acba0ca47c3e
Alessandro <77743955+ale-006@users.noreply.github.com>
2022-03-03 19:19:04 +0100

    Update short.js

--- commands/short.js
+++ commands/short.js
@@ -10,9 +10,13 @@ module.exports = {
         name: "short"
     },
     async execute(interaction) {
+        try {
         if(!validUrl.isUri(interaction.options.getString("url"))) return interaction.reply({content: "Please, provide a valid URL.", ephemeral: true})
         if(db.has(interaction.options.getString("name"))) return interaction.reply({content: "This URL name was already taken!", ephemeral: true})
         db.set(interaction.options.getString("name"), interaction.options.getString("url"))
         interaction.reply({content: "<a:arrow:895720936312033281> **__SUCCESS!__**\nYour URL was successfully created!\n<https://easyshort.gq/" + interaction.options.getString("name") + ">", ephemeral: true})
+        } catch (err) {
+            console.log(err)
+        }
     }
 }

aa3d79733db42bf268ab4826d5d719bcb332367d
Alessandro <77743955+ale-006@users.noreply.github.com>
2022-03-03 19:19:38 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -9,7 +9,11 @@ module.exports = {
         name: "stats"
     },
     async execute(interaction) {
+        try {
         let msg = "<a:arrow:895720936312033281> **__STATS__**\n\n__Shortened links:__ " + db.all().filter(a => a.ID != "website_access" && a.ID != "executed_cmd").length + "\n__Website access:__ " + db.get("website_access") + "\n__Executed commands:__ " + db.get("executed_cmd")
         interaction.reply({content: msg, ephemeral: true})
+        } catch (err) {
+            console.log(err)
+        }
     }
 }
